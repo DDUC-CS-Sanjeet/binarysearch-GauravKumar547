539c7555b64048e6c976555b08365f0935c3d157
GauravKumar547 <gauravkhanna547@gmail.com>
2020-01-17 17:02:07 +0530

    Binary search

--- BinSearch.cpp
+++ BinSearch.cpp
@@ -1,18 +1,87 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
 
 bool binarySearchReacursive(int* array, int startIndex, int lastIndex, int element)
 {
-  return true;
+	 int mid=(startIndex+lastIndex)/2;
+	 if(startIndex<=lastIndex)
+	 {
+	 	if(element==array[mid])
+        {
+      	   return true;
+	    }
+	    else if(array[mid]<element)
+        {
+             return(array,mid+1,lastIndex,element);
+        }
+       else
+        {
+            return(array,startIndex,mid-1,element);
+        }
+    
+	 } 
+	 else
+	 {
+	 	return false;
+	 } 
+     
 }
 
 bool binarySearchIterative(int* array, int startIndex, int lastIndex, int element)
 {
-  return true;
+	 int mid=0;
+     while(startIndex<=lastIndex)
+      {             
+         mid=(startIndex+lastIndex)/2;
+         if(array[mid]<element)
+            {
+                 startIndex=mid+1;
+            }
+             else
+             {
+                 lastIndex=mid-1;
+             }
+         if(element==array[mid])
+           {
+      	     return true;
+	       }
+       }
+       return false;
+     
+ 
 }
 
 int main()
 {
+	int element;
+	int size;
+	cout<<"Enter the size of the array: ";
+	cin>>size;
+	int arr[size];
+	cout<<"Enter the elements of array: ";
+	for(int i=0;i<size;i++)
+	cin>>arr[i];
+	sort(arr,arr+size);
+	cout<<"Enter the element to search: ";
+	cin>>element;
+	if(binarySearchIterative(arr,0,size,element))
+	{
+		cout<<"\nThe element is found using iteration.";
+	}
+	else
+	{
+		cout<<"The element is not found using iteration.";
+	}
+	if(binarySearchReacursive(arr,0,size,element))
+	{
+		cout<<"\nThe element is found using recursion.";
+	}
+	else
+	{
+		cout<<"The element is not found using recursion.";
+	}
+	
 
 
 
